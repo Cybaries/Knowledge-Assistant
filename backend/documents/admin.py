9186dd36f4c415b9b7a429fc2fa92ebd5986c179
Cybaries <amanjha4753@gmail.com>
2026-08-06 02:20:43 +0530

    created document, documentChunks models

--- backend/documents/admin.py
+++ backend/documents/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Document, DocumentChunk
+
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "owner",
+        "status",
+        "uploaded_at",
+    )
+
+    search_fields = (
+        "title",
+        "owner__username",
+    )
+
+    list_filter = (
+        "status",
+        "uploaded_at",
+    )
+
+
+@admin.register(DocumentChunk)
+class DocumentChunkAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "document",
+        "created_at",
+    )
--- backend/documents/models.py
+++ backend/documents/models.py
@@ -1,3 +1,48 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+class DocumentStatus(models.TextChoices):
+    UPLOADING = "UPLOADING", "Uploading"
+    PROCESSING = "PROCESSING", "Processing"
+    READY = "READY", "Ready"
+    FAILED = "FAILED", "Failed"
+class Document(models.Model):
+    title = models.CharField(max_length=255)
+    file = models.FileField(upload_to="documents/")
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="documents"
+    )
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=20,
+        choices=DocumentStatus.choices,
+        default=DocumentStatus.UPLOADING,
+    )
+
+    def __str__(self):
+        return self.title
+    class Meta:
+     ordering = ["-uploaded_at"]
+
+class DocumentChunk(models.Model):
+    document = models.ForeignKey(
+        Document,
+        on_delete=models.CASCADE,
+        related_name="chunks"
+    )
+    text = models.TextField()
+    embedding = models.JSONField(
+        null=True,
+        blank=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"{self.document.title} - Chunk {self.pk}"
+    class Meta:
+        ordering = ["created_at"]
+
+    
